670dd4ae4b32b76eda926499a4922050437c1aa7
Mara Caoile <mara.caoile@gmail.com>
2023-11-27 20:02:46 -0600

    created functioning game form and page

--- utils/data/gameData.js
+++ utils/data/gameData.js
@@ -7,5 +7,30 @@ const getGames = () => new Promise((resolve, reject) => {
     .catch(reject);
 });
 
+const createGame = (game) => new Promise((resolve, reject) => {
+  fetch(`${clientCredentials.databaseURL}/games`, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(game),
+  })
+    .then((response) => response.json())
+    .then((data) => {
+      resolve(data);
+    })
+    .catch((error) => {
+      console.error('Create Game Error:', error);
+      reject(error);
+    });
+});
+
+const getGameTypes = () => new Promise((resolve, reject) => {
+  fetch(`${clientCredentials.databaseURL}/gametypes`)
+    .then((response) => response.json())
+    .then(resolve)
+    .catch(reject);
+});
+
 // eslint-disable-next-line import/prefer-default-export
-export { getGames };
+export { getGames, createGame, getGameTypes };
